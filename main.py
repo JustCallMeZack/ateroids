f10c8a23d0f980bad9758aead50202f625de4b14
JustCallMeZack <Zletts100@gmail.com>
2025-05-16 00:13:52 -0400

    Added player class and created triangle drawing

--- main.py
+++ main.py
@@ -4,18 +4,29 @@
 import pygame
 
 from constants import *
+from player import *
+from circleshape import *
 
 
 
 def main():
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
+
+
+    # Game loop
+    while True: 
         screen.fill(000)
+        player.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60)/1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
